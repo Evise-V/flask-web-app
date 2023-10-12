8ddca8821230623f0b6c9dd2c6656f55917b0a0a
Evise-V <evise_@outlook.com>
2023-10-12 13:22:08 -0700

    Added virtual enviromnet(venv) and requirements.txt file for managing packages

--- main.py
+++ main.py
@@ -3,4 +3,18 @@ from website import create_app
 app=create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+     app.run(debug=True)
+
+# running options
+#  https://flask.palletsprojects.com/en/3.0.x/cli/
+#
+# - run flask in production mode (debug if off) on port 8000
+#   flask --app main  run -p 8000         
+#
+# - run flask in development mode (debug is on) on port 8000
+#   flask --app main  --debug run -p 8000 
+#
+# - run flask using env variables
+#   $env:FLASK_APP="main"
+#   $env:FLASK_ENV="development"
+#   flask run
